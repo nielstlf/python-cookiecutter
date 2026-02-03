9183a48ffb4f815d130b6dbf17ecc955af01185c
Adam Tyson <adam@adamltyson.com>
2026-02-03 12:52:54 +0000

    ignore docutils url in linkcheck

--- {{cookiecutter.package_name}}/docs/source/conf.py
+++ {{cookiecutter.package_name}}/docs/source/conf.py
@@ -122,3 +122,6 @@ sitemap_url_scheme = "{link}"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+
+linkcheck_ignore = ["https://docutils.sourceforge.io/*"] # to avoid frequent 403 error
